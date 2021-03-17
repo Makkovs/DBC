1ce29635c795315dfdb1fb27866deaa6431fc908
Makkovs <kovalmax09@gmail.com>
2021-03-17 19:30:26 +0200

    New styles

--- init.py
+++ init.py
@@ -1,16 +1,20 @@
 import eel
 import sqlite3
 import os
+import json
 
-eel.init('C:\\Users\\Makkov\\Desktop\\DBC')
+eel.init('C:\\Users\\Makkov\\Desktop\\Python\\DBC\\gui')
 
-@eel.expose
-def create(name, path, platform, program):
-    jp = {"j2" : "bot.js", "p2" : "bot.py"}
-    filepath = os.path.join(path, jp[program])
-    if not os.path.exists(path):
-        os.makedirs(path)
-    f = open(filepath, "a")
+@eel.expose 
+def create(name, platform, program): #созданиие проекта
+    jp = {"j2" : "bot.js", "p2" : "bot.py"} #словарь языки файл
+    filepath = os.path.join(f'.\\projects\\{name}', jp[program]) #создание файла
+    if not os.path.exists(f'.\\projects\\{name}'): 
+        os.makedirs(f'.\\projects\\{name}')
+    f = open(filepath, "a") 
+    filepath = os.path.join(f'.\\projects\\{name}', f"settings.json") #settinsg.json создание
+    if not os.path.exists(f'.\\projects\\{name}'):
+        os.makedirs(f'.\\projects\\{name}') 
+    jsonf = open(filepath, "a")
 
-
-eel.start('index.html')
+eel.start('index.html') #ell start
